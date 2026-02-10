306341290f7997ff49f21f0eceb412cb3caf1635
Zbyněk Jiráček <zbynek.jiracek@gmail.com>
2026-02-10 20:30:47 +0100

    Added missing Wgs.cs

---
+++ AswModel/JR_XML_EXP/Wgs.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace JR_XML_EXP
+{
+    public class Wgs
+    {
+        [XmlAttribute("lat")]
+        public double Lat;
+
+        [XmlAttribute("lon")]
+        public double Lon;
+    }
+}
